4f5a86f39a01c6f0c514bca5472d5fd12528445c
Pondingz <pawarut.klaiarmon@gmail.com>
2024-09-05 23:21:11 +0700

    init

---
+++ src/main/java/com/allianz/AllianzInterviewBeApplication.java
@@ -0,0 +1,13 @@
+package com.allianz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllianzInterviewBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllianzInterviewBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allianz/AllianzInterviewBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllianzInterviewBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
